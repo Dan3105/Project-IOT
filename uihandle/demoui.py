1dc79efc076f7c1129dd6045a847de20036a4e7b
yuchud <studywithhuy2002@gmail.com>
2024-01-13 11:06:17 +0700

    handle input

--- uihandle/demoui.py
+++ uihandle/demoui.py
@@ -1,7 +1,7 @@
 import os.path
 import datetime
 import pickle
-
+import time
 import tkinter as tk
 import cv2
 from PIL import Image, ImageTk
@@ -9,26 +9,28 @@ import util
 import torch
 from model_handler import ModelAntiSpoffing, ModelRecognition
 import os
+from tkinter import messagebox
 
 CRR_PATH = os.curdir
 
 MODEL_RECOG_PATH = 'model-2/face_recognizer_fast.onnx'
 MODEL_DETECT_PATH = 'model-2/yunet_s_640_640.onnx'
-MODEL_ANTI_PATH = 'uihandle/best.pt'
+MODEL_ANTI_PATH = 'antispoof.pt'
 
-DB_IMAGE_PATH = 'uihandle/db/image-data' 
-DB_CSV_PATH = 'uihandle/db/db.csv'
+DB_IMAGE_PATH = 'db/image-data'
+DB_CSV_PATH = 'db/db.csv'
 
 class App:
     def __init__(self):
+        self.allow_detect = True
+
         self.main_window = tk.Tk()
         self.main_window.geometry("1200x520")
+        # self.login_button_main_window = util.get_button(self.main_window, 'login', 'green', self.login)
+        # self.login_button_main_window.place(x=750, y=200)
 
-        #self.login_button_main_window = util.get_button(self.main_window, 'login', 'green', self.login)
-        #self.login_button_main_window.place(x=750, y=200)
-
-        #self.logout_button_main_window = util.get_button(self.main_window, 'logout', 'red', self.logout)
-        #self.logout_button_main_window.place(x=750, y=300)
+        # self.logout_button_main_window = util.get_button(self.main_window, 'logout', 'red', self.logout)
+        # self.logout_button_main_window.place(x=750, y=300)
 
         self.register_new_user_button_main_window = util.get_button(self.main_window, 'register new user', 'gray',
                                                                     self.register_new_user, fg='black')
@@ -50,35 +52,38 @@ class App:
         # self.log_path = './log.txt'
 
     def add_webcam(self, label):
-        #phan nay nhan cam
+        # phan nay nhan cam
         self.cap = cv2.VideoCapture(0)
 
         self._label = label
         self.process_webcam()
 
     def process_webcam(self):
-        ret, frame = self.cap.read()
-        if frame is not None:
+        if self.allow_detect:
 
-            #Handle logic cua mo hay dong
-            
-            #Mat real, Mat fake
-            image_moded, face = self.anti_spoof_model.detect(frame)
-            if face is not None:
-                #neu mat real
-                #ten nguoi dung
-                result_name = self.model_recog.predict(face)
-                if result_name is not None:
-                    # lam gi thi lam
-                    print(result_name)
-            self.most_recent_capture_arr = image_moded
-            img_ = cv2.cvtColor(self.most_recent_capture_arr, cv2.COLOR_BGR2RGB)
-            self.most_recent_capture_pil = Image.fromarray(img_)
-            imgtk = ImageTk.PhotoImage(image=self.most_recent_capture_pil)
-            self._label.imgtk = imgtk
-            self._label.configure(image=imgtk)
+            ret, frame = self.cap.read()
+            if frame is not None:
 
-            self._label.after(20, self.process_webcam)
+                # Handle logic cua mo hay dong
+
+                # Mat real, Mat fake
+                image_moded, face = self.anti_spoof_model.detect(frame)
+                if face is not None:
+                    # neu mat real
+                    # ten nguoi dung
+                    result_name = self.model_recog.predict(face)
+                    if result_name is not None:
+                        # lam gi thi lam
+                        print(result_name)
+
+                self.most_recent_capture_arr = image_moded
+                img_ = cv2.cvtColor(self.most_recent_capture_arr, cv2.COLOR_BGR2RGB)
+                self.most_recent_capture_pil = Image.fromarray(img_)
+                imgtk = ImageTk.PhotoImage(image=self.most_recent_capture_pil)
+                self._label.imgtk = imgtk
+                self._label.configure(image=imgtk)
+
+        self._label.after(20, self.process_webcam)
 
     def login(self):
         pass
@@ -86,15 +91,37 @@ class App:
     def logout(self):
         pass
 
+    def validate_text(self, event):
+        start = "1.0"
+        end = "end-1c"
+        text_content = self.entry_text_register_new_user.get(start, end)
+        print(text_content)
+        if text_content and not text_content[-1].isalnum():
+            messagebox.showwarning("Invalid Entry", "Only allow letters and numbers")
+
+            # Remove the last entered character (punctuation)
+            self.entry_text_register_new_user.delete("end-2c")
+
+    def on_closing(self, event):
+        self.main_window.deiconify()
+        self.allow_detect = True
+
+    def on_openning(self, event):
+        self.main_window.withdraw()
+        self.allow_detect = False
 
     def register_new_user(self):
         self.register_new_user_window = tk.Toplevel(self.main_window)
         self.register_new_user_window.geometry("1200x520+370+120")
-
-        self.accept_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Accept', 'green', self.accept_register_new_user)
+        self.register_new_user_window.bind("<Visibility>", self.on_openning)
+        self.register_new_user_window.bind("<Destroy>", self.on_closing)
+        #self.register_new_user_window.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.accept_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Accept', 'green',
+                                                                      self.accept_register_new_user)
         self.accept_button_register_new_user_window.place(x=750, y=300)
 
-        self.try_again_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Try again', 'red', self.try_again_register_new_user)
+        self.try_again_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Try again',
+                                                                         'red', self.try_again_register_new_user)
         self.try_again_button_register_new_user_window.place(x=750, y=400)
 
         self.capture_label = util.get_img_label(self.register_new_user_window)
@@ -103,9 +130,11 @@ class App:
         self.add_img_to_label(self.capture_label)
 
         self.entry_text_register_new_user = util.get_entry_text(self.register_new_user_window)
+        self.entry_text_register_new_user.bind('<KeyRelease>', self.validate_text)
         self.entry_text_register_new_user.place(x=750, y=150)
 
-        self.text_label_register_new_user = util.get_text_label(self.register_new_user_window, 'Please, \ninput username:')
+        self.text_label_register_new_user = util.get_text_label(self.register_new_user_window,
+                                                                'Please, \ninput username:')
         self.text_label_register_new_user.place(x=750, y=70)
 
     def try_again_register_new_user(self):
@@ -125,7 +154,7 @@ class App:
         ret, frame = self.cap.read()
         name = self.entry_text_register_new_user.get(1.0, "end-1c")
         image = frame
-        
+
         try:
             _, face_human = self.anti_spoof_model.detect(image)
             self.model_recog.save_data_user(face_human, name)
